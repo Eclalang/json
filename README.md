3c4e51495a14b18922093fa95d5b6055db3524fa
axel senecal <senecal.axel89@gmail.com>
2024-01-10 16:43:38 +0100

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## JSON LIBRARY FOR ECLA
+# Json library
 
-# Candidate functions :
+## Candidate functions :
 
-| Func Name |            Prototype             |                Description                 | Comments |
-|:---------:|:--------------------------------:|:------------------------------------------:|:--------:|
-|  Marshal  |  Marshal(content any) string {}  |     Returns the JSON encoding of a map     |   N/A    |
-| Unmarshal | Unmarshal(content string) any {} | Returns the map representation of the JSON |   N/A    |
+| Func Name |                     Prototype                      |                Description                 | Comments |
+|:---------:|:--------------------------------------------------:|:------------------------------------------:|:--------:|
+|  Marshal  |      Marshal(map\[string]\[string]) string {}      |     Returns the JSON encoding of a map     |   N/A    |
+| Unmarshal | Unmarshal(content string) map\[string]\[string] {} | Returns the map representation of the JSON |   N/A    |
--- json_test.go
+++ json_test.go
@@ -2,8 +2,9 @@ package jsonlibtest
 
 import (
 	"encoding/json"
+	"fmt"
 	PackageJson "github.com/Eclalang/json"
-	"io/ioutil"
+	"io"
 	"os"
 	"reflect"
 	"testing"
@@ -11,9 +12,10 @@ import (
 
 func TestMarshal(t *testing.T) {
 	// Marshal map
-	maps := map[string]interface{}{"name": "Ecla", "members": 7, "language": "Go"}
+	maps := map[string]string{"name": "Ecla", "members": "7", "language": "Go"}
 	expected, _ := json.Marshal(maps)
 	actual := PackageJson.Marshal(maps)
+	fmt.Println(actual)
 	if string(expected) != actual {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
@@ -32,7 +34,7 @@ func TestUnmarshal(t *testing.T) {
 	var expected map[string]interface{}
 	file, _ := os.Open("test.json")
 	defer file.Close()
-	fileContent, _ := ioutil.ReadAll(file)
+	fileContent, _ := io.ReadAll(file)
 	_ = json.Unmarshal(fileContent, &expected)
 	actual := PackageJson.Unmarshal(string(fileContent))
 	if !reflect.DeepEqual(expected, actual) {
@@ -43,7 +45,7 @@ func TestUnmarshal(t *testing.T) {
 	var expect []map[string]interface{}
 	file2, _ := os.Open("test2.json")
 	defer file2.Close()
-	fileContent2, _ := ioutil.ReadAll(file2)
+	fileContent2, _ := io.ReadAll(file2)
 	_ = json.Unmarshal(fileContent2, &expect)
 	actual = PackageJson.Unmarshal(string(fileContent2))
 	if !reflect.DeepEqual(expect, actual) {
